cc706739b6578aaa4613272558a853dea0e0ea05
Hasan Basri Akçay <hasan.basri.akcay@gmail.com>
2022-08-29 18:42:52 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="featdist",
-    version="v0.0.2",
+    version="v0.0.3",
     author="Hasan Basri Akcay",
     author_email="hasan.basri.akcay@gmail.com",
     description="Train test target distribution function for machine learning",
