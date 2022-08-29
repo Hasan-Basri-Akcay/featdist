5c5fa54548409b426bb37352ee961cea8719a841
Hasan Basri Akçay <hasan.basri.akcay@gmail.com>
2022-08-29 16:51:24 +0300

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="featdist",
+    version="v0.0.1",
+    author="Hasan Basri Akcay",
+    author_email="hasan.basri.akcay@gmail.com",
+    description="Train test target distribution function for machine learning",
+    long_description=(
+        "Featdist (Train Test Target Distribution) helps with feature understanding, "
+        "calculating feature importances, feature comparison, feature debugging, and "
+        "leakage detection"
+    ),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hasan-Basri-Akcay/featdist",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["pandas", "numpy", "matplotlib"],
+    keywords=["python", "data science", "data analysis", "exploratory data analysis", 
+              "distribution", "beginner"],
+)
